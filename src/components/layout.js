17138e8427f549715479682493026d410857e105
arunvisvajeetrs <arunvisvajeet@gmail.com>
2025-04-25 18:01:04 +0200

    feat: test

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,7 +39,7 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
-          {` `}
+          {`sdf `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
